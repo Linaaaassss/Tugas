f3c40ba06dd8f51c24429372413220ee84474154
Linaaaassss <145944061+Linaaaassss@users.noreply.github.com>
2024-03-30 21:24:18 +0800

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,73 @@
+nm="Melina Rahmawati"
+ttl= "Lanji, 21 mei 2005"
+umur= "18 tahun 10 bulan"
+alamat="Dusun Lanji, Desa Darmaji, Kec. Kopang, Kab. Loteng."
+jenkel="Perempuan"
+agama="Islam"
+hobi= "Membaca novel atau cerita"
+tb="160 cm"
+bb= "41 kg"
+sd="SD Negeri 1 Darmaji"
+smp="SMP Negeri 2 Kopang"
+sma= "SMA Negeri 1 Praya Tengah"
+sts="Mahasiswa"
+pg= "STMIK LOMBOK"
+aktn= int(2023)
+prodi="Teknik Informatika"
+smt= int (2)
+nim= "TI20230029"
+wa=int(6281936999288)
+ig="its_na05"
+tt="kangolengnih"
+eml="xlyntaradeeva@gmail.com"
+
+
+
+print("===="*21)
+print ( " ","----"*7," BIODATA PRIBADI ","----"*8)
+print("===="*21)
+print (" ")
+print (" Nama                       : ", nm)
+print (" Tempat & Tanggal Lahir     : ", ttl)
+print (" Umur                       : ", umur)
+print (" Alamat                     : ",alamat)
+print (" Jenis Kelamin              : ", jenkel)
+print (" Agama                      : ", agama)
+print (" Hobi                       : ", hobi)
+print (" Tinggi Badan               : ", tb)
+print (" Berat Badan                : ", bb)
+print (" Status                     : ", sts)
+print (" ")
+
+print("===="*21)
+print (" ","----"*8," PENDIDIKAN ","----"*8)
+print("===="*21)
+print (" ")
+print (" Sekolah Dasar              : ", sd)
+print (" Sekolah menengah pertama   : ", smp)
+print (" Sekolah menengah atas      : ", sma)
+print (" Perguruan Tiggi            : ", pg)
+print (" Program Studi              : ", prodi)
+print (" Nim                        : ", nim)
+print (" Semester                   : ", smt)
+print (" Angkatan                   : ", aktn)
+print (" ")
+
+
+print("===="*21)
+print (" ","----"*8," SOSIAL MEDIA ","----"*8)
+print("===="*21)
+print (" ")
+print (" Whastsapp                  : ", wa)
+print (" Instagram                  : ", ig)
+print (" Tiktok                     : ", tt)
+print (" Email                      : ", eml)
+print (" ")
+
+
+print("===="*21)
+print (" ","---"*2,"TERIMAKASIH SUDAH MEMBACA, DON'T FORGET TO FOLLOW MY SOCIAL MEDIA","---"*2)
+print (" ","----"*7, "SALAM MANIS PENULIS","----"*7,)
+print("===="*21)
+       
+
